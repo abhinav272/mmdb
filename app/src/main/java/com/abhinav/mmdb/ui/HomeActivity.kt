c98253f2f9f37fc34cef011cbbc6d7f3fa22b407
abhinav272 <abhinav.ih@gmail.com>
2019-05-22 18:53:49 +0530

    launching screen created

--- app/src/main/java/com/abhinav/mmdb/ui/HomeActivity.kt
+++ app/src/main/java/com/abhinav/mmdb/ui/HomeActivity.kt
@@ -1,9 +1,10 @@
-package com.abhinav.mmdb
+package com.abhinav.mmdb.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abhinav.mmdb.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/abhinav/mmdb/ui/OnBoardActivity.kt
+++ app/src/main/java/com/abhinav/mmdb/ui/OnBoardActivity.kt
@@ -1,13 +1,36 @@
 package com.abhinav.mmdb.ui
 
+import android.content.Intent
 import android.os.Bundle
 import com.abhinav.mmdb.R
+import kotlinx.coroutines.*
 
-class OnBoardActivity: BaseActivity() {
+class OnBoardActivity : BaseActivity() {
+
+    var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_on_boarding)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        job = GlobalScope.launch {
+            delay(2000)
+            withContext(Dispatchers.Main) {
+                showHomeActivity().apply { finish() }
+            }
+        }
+    }
+
+    private fun showHomeActivity() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        job?.cancel()
     }
 }
