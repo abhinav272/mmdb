cd94256d8d1901e97a333354e92107e1addc5d51
abhinav272 <abhinav.ih@gmail.com>
2019-05-22 16:39:17 +0530

    UI files

---
+++ app/src/main/java/com/abhinav/mmdb/ui/BaseActivity.kt
@@ -0,0 +1,8 @@
+package com.abhinav.mmdb.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity() {
+
+
+}
---
+++ app/src/main/java/com/abhinav/mmdb/ui/OnBoardActivity.kt
@@ -0,0 +1,13 @@
+package com.abhinav.mmdb.ui
+
+import android.os.Bundle
+import com.abhinav.mmdb.R
+
+class OnBoardActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
