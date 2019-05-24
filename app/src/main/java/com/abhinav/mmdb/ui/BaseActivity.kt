eed156b6674a1a69dba42c57464278ae8a118386
abhinav272 <abhinav.ih@gmail.com>
2019-05-24 22:00:31 +0530

    common method added for fragments

--- app/src/main/java/com/abhinav/mmdb/ui/BaseActivity.kt
+++ app/src/main/java/com/abhinav/mmdb/ui/BaseActivity.kt
@@ -1,11 +1,20 @@
 package com.abhinav.mmdb.ui
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.abhinav.mmdb.R
 import com.abhinav.mmdb.data.model.Result
 import com.abhinav.mmdb.showToastMsg
 import com.abhinav.mmdb.utils.Event
 
 open class BaseActivity : AppCompatActivity() {
+
+    fun addFragment(containerId: Int, fragment: BaseFragment) {
+        supportFragmentManager.commit {
+            addToBackStack(fragment::class.java.simpleName)
+            add(containerId, fragment, fragment::class.java.simpleName)
+        }
+    }
 
     fun onFailure(event: Event<Result.Failure>?) {
         if (!event?.isAlreadyHandled!!) {
