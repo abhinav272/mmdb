09c26de2ac862afe5b8800e84a8122478fa2fbc1
abhinav272 <abhinav.ih@gmail.com>
2019-05-26 15:23:31 +0530

    homefragment refactored

--- app/src/main/java/com/abhinav/mmdb/ui/home/HomeFragment.kt
+++ app/src/main/java/com/abhinav/mmdb/ui/home/HomeFragment.kt
@@ -1,30 +1,64 @@
 package com.abhinav.mmdb.ui.home
 
+//import com.abhinav.mmdb.ui.adapters.TrendingItemsAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import com.abhinav.mmdb.R
+import com.abhinav.mmdb.inflate
 import com.abhinav.mmdb.ui.BaseFragment
+import com.abhinav.mmdb.ui.adapters.HomeItemsAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment: BaseFragment() {
+class HomeFragment : BaseFragment() {
 
-    val viewModel by activityViewModels<HomeViewModel>()
+    private lateinit var gridLayoutManager: GridLayoutManager
+    private lateinit var homeItemsAdapter: HomeItemsAdapter
+    private val viewModel by activityViewModels<HomeViewModel>()
 
-    companion object{
+    companion object {
         fun getInstance() = HomeFragment()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        homeItemsAdapter = HomeItemsAdapter()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return container?.inflate(R.layout.fragment_home)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.trendingLiveData.observe(this, Observer { it.forEach { it1 -> it1.name.let { s -> println(s) } } })
+
+        bindTrendingAdapter()
+
+        viewModel.trendingLiveData.observe(this, Observer {
+            homeItemsAdapter.onTrendingItemsLoaded(it)
+        })
         viewModel.fetchTrendingItems()
+    }
+
+    private fun bindTrendingAdapter() {
+        gridLayoutManager = GridLayoutManager(rv_items.context, 2)
+        gridLayoutManager.apply {
+            spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int = when (homeItemsAdapter.getItemViewType(position)) {
+                    homeItemsAdapter.TRENDING_ITEMS -> 2
+                    else -> 1
+                }
+            }
+        }
+
+        rv_items.apply {
+            adapter = homeItemsAdapter
+            layoutManager = gridLayoutManager
+        }
     }
 
 }
